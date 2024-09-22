59a70e3f08a25ecc185fcce03767ebf2abe66c17
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-09-22 19:26:11 -0300

    fix: update type names

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,6 @@ import type { z } from 'zod'
 
 import type { movieSchema, searchMovieWithRatingSchema } from '@/schemas/movie'
 
-export type SearchMovieWithRatingSchema = z.infer<typeof searchMovieWithRatingSchema>
+export type SearchMovieWithRating = z.infer<typeof searchMovieWithRatingSchema>
 
 export type Movie = z.infer<typeof movieSchema>
