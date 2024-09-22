96c897dd4c884cd133634f26296bcbd846a0ae4c
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-09-22 19:22:43 -0300

    feat: add custom not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import sadPopcorn from '../../public/sad-popcorn.svg'
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center py-8 text-center">
+      <h2 className="text-3xl font-semibold">Not Found</h2>
+      <p className="text-muted-foreground text-lg">Could not find the movie you are looking for.</p>
+      <Image
+        alt="Sad popcorn"
+        height={300}
+        src={sadPopcorn}
+        width={300}
+      />
+      <Link
+        className="font-semibold"
+        href="/"
+      >
+        Return Home
+      </Link>
+    </div>
+  )
+}
