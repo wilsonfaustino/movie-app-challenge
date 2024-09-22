a0242ef0ee7eca94d3e72400488dbbd661a18061
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-09-22 19:22:01 -0300

    refactor: improve layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 
 import type { Metadata } from 'next'
 
+import { Header } from '@/components/app/header'
+
 export const metadata: Metadata = {
   title: 'Jaya Movie App',
   description: 'Jaya Movie App',
@@ -14,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`min-h-screen antialiased`}>
+        <div className="flex min-h-screen w-full">
+          <div className="mx-auto max-w-7xl flex-1 px-4 py-10 xl:px-0">
+            <Header />
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
