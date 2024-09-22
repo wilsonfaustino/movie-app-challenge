fe5bd7ba0c95dfc4d070b01d649707b3c08c9a55
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-09-22 19:24:32 -0300

    refactor: improve layout in MovieCard component

--- src/components/app/movie-card.tsx
+++ src/components/app/movie-card.tsx
@@ -1,26 +1,32 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
-import type { SearchMovieWithRatingSchema } from '@/types'
+import type { SearchMovieWithRating } from '@/types'
 
 import { Card } from '../ui/card'
 
-export function MovieCard({ movie }: { movie: SearchMovieWithRatingSchema }) {
+export function MovieCard({ movie }: { movie: SearchMovieWithRating }) {
   return (
-    <Card className="flex gap-2 overflow-hidden">
+    <Card className="flex max-h-36 gap-2 overflow-hidden">
       <div className="flex-1">
         <Card.Header>
-          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Title className="text-lg">{movie.Title}</Card.Title>
           <Card.Description>{`Year: ${movie.Year}`}</Card.Description>
         </Card.Header>
 
         <Card.Footer>{`Rating: ${movie.imdbRating}`}</Card.Footer>
       </div>
-      <Image
-        alt={movie.Title}
-        height={100}
-        src={movie.Poster}
-        width={100}
-      />
+      <div className="aspect-poster h-full">
+        <Link href={`/${movie.imdbID}`}>
+          <Image
+            alt={movie.Title}
+            className="h-full w-auto object-cover"
+            height={100}
+            src={movie.Poster}
+            width={100}
+          />
+        </Link>
+      </div>
     </Card>
   )
 }
