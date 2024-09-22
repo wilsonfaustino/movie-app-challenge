d876e87bb59c201688bcdfc15debf37d6849cf0f
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-09-22 12:24:55 -0300

    feat: update getMovies to include ratings

--- src/actions/get-movies.ts
+++ src/actions/get-movies.ts
@@ -1,18 +1,38 @@
 import { env } from '@/env'
-import { movieSchema } from '@/schemas/movie'
+import { searchMovieSchema, searchMovieWithRatingSchema } from '@/schemas/movie'
 
 export async function getMovies(search: string) {
   const res = await fetch(`${env.API_URL}?apikey=${env.API_KEY}&s=${search}`)
   const data = await res.json()
 
-  const parsedData = movieSchema.array().safeParse(data.Search)
+  const parsedData = searchMovieSchema.array().safeParse(data.Search)
 
   if (!parsedData.success) {
     throw new Error('Failed to parse movies')
   }
 
+  const moviesWithRatings = await Promise.all(
+    parsedData.data.map(async (movie) => {
+      const res = await fetch(`${env.API_URL}?apikey=${env.API_KEY}&i=${movie.imdbID}`)
+      const data = await res.json()
+
+      const parsedData = searchMovieWithRatingSchema.safeParse(data)
+
+      if (!parsedData.success) {
+        throw new Error('Failed to parse movie')
+      }
+
+      return {
+        ...movie,
+        imdbRating: data.imdbRating,
+      }
+    }),
+  )
+
+  console.log(moviesWithRatings)
+
   return {
-    movies: parsedData.data,
+    movies: moviesWithRatings,
     totalResults: data.totalResults,
   }
 }
