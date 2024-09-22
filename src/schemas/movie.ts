d5e0e1e2268adb4447abbe9144d11391163b60b7
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-09-22 12:20:49 -0300

    feat: update schema for movies

--- src/schemas/movie.ts
+++ src/schemas/movie.ts
@@ -1,11 +1,48 @@
 import { z } from 'zod'
 
+const ratingSchema = z.object({
+  Source: z.string(),
+  Value: z.string(),
+})
+
 export const movieSchema = z.object({
   Title: z.string(),
   Year: z.string(),
+  Rated: z.string(),
+  Released: z.string(),
+  Runtime: z.string(),
+  Genre: z.string(),
+  Director: z.string(),
+  Writer: z.string(),
+  Actors: z.string(),
+  Plot: z.string(),
+  Language: z.string(),
+  Country: z.string(),
+  Awards: z.string(),
+  Poster: z.string(),
+  Ratings: z.array(ratingSchema),
+  Metascore: z.string(),
+  imdbRating: z.string(),
+  imdbVotes: z.string(),
   imdbID: z.string(),
   Type: z.enum(['movie', 'series', 'episode']),
-  Poster: z.string().url(),
+  DVD: z.string(),
+  BoxOffice: z.string(),
+  Production: z.string(),
+  Website: z.string(),
+  Response: z.string(),
 })
 
-export type Movie = z.infer<typeof movieSchema>
+export const searchMovieSchema = movieSchema.pick({
+  Title: true,
+  Year: true,
+  imdbID: true,
+  Type: true,
+  Poster: true,
+})
+
+const movieRatingSchema = movieSchema.pick({
+  imdbRating: true,
+})
+
+export const searchMovieWithRatingSchema = searchMovieSchema.merge(movieRatingSchema)
