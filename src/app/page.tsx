2b7b59bd8d54b99ccc4cb9556589e4e05a3f0563
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-09-23 12:29:48 -0300

    refactor: update page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,34 @@
-import Image from 'next/image'
+import { Suspense } from 'react'
 
 import { getMovies } from '@/actions/get-movies'
-import { ListComponent } from '@/components/app/list-component'
-import { MovieCard } from '@/components/app/movie-card'
+import { MoviesList } from '@/components/app/movies-list'
+import { MoviesSkeleton } from '@/components/app/movies-skeleton'
+import { Pagination } from '@/components/app/pagination'
 import { searchParamsCache } from '@/lib/search-params'
-import type { SearchMovieWithRating } from '@/types'
-
-import sadPopcorn from '../../public/sad-popcorn.svg'
 
 export default async function Home({ searchParams }: { searchParams: Record<string, string | string[] | undefined> }) {
-  const { search } = searchParamsCache.parse(searchParams)
-  const { movies, totalResults } = await getMovies(search)
+  const { search, page } = searchParamsCache.parse(searchParams)
+  const { totalResults } = await getMovies(search)
 
   if (!search) {
     return <div className="py-8 text-center">Please enter a search term</div>
   }
 
-  if (totalResults === 0) {
-    return (
-      <div className="flex flex-col items-center justify-center py-8 text-center">
-        <div>No results found for &quot;{search}&quot;</div>
-        <Image
-          alt="Sad popcorn"
-          height={300}
-          src={sadPopcorn}
-          width={300}
-        />
-      </div>
-    )
-  }
-
   return (
     <div>
-      <h2 className="mb-4 text-xl font-semibold">Search results for &quot;{search}&quot;</h2>
-      <p className="mb-4">Total results: {totalResults}</p>
-      <ListComponent<SearchMovieWithRating>
-        className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3"
-        data={movies}
-        renderItem={(movie) => (
-          <MovieCard
-            key={movie.imdbID}
-            movie={movie}
-          />
-        )}
-      />
+      <h2 className="text-muted-foreground my-4 ml-auto text-center text-xl font-semibold">
+        Search results for &quot;{search}&quot;
+      </h2>
+      <Suspense
+        fallback={<MoviesSkeleton />}
+        key={`search-${search}-page-${page}`}
+      >
+        <MoviesList
+          page={page}
+          query={search}
+        />
+      </Suspense>
+      <Pagination totalResults={totalResults} />
     </div>
   )
 }
